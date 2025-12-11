ab50fc013235631b4103aa60a4ce7a39d5b7b7db
its-shahzaib <shahzaibkhana2005@gmail.com>
2025-12-11 11:29:12 +0500

    Delete ai_recommend.js

--- ai_recommend.js
+++
@@ -1,6 +0,0 @@
-function recommendProducts(productId){
-    let product = products.find(p => p.id === productId);
-    let related = products.filter(p => p.category === product.category && p.id !== productId);
-    
-    console.log("AI Recommended:", related);
-}
